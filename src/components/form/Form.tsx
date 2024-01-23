d1668586af87a93383adf914c8bd1a5444badd31
artemigla <igla.artem@gmail.com>
2024-01-24 01:16:36 +0200

    Input field validation is implemented.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Button } from '../button/Button';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { IFormValue } from '../../interface/interfaces';
 import './style.css';
 
-
 export const Form: React.FC = () => {
 
-    const { register, handleSubmit } = useForm<IFormValue>();
+    const { register, handleSubmit, formState: { errors } } = useForm<IFormValue>({
+        mode: 'onChange'
+    });
 
     const onSubmit: SubmitHandler<IFormValue> = (data) => console.log(data);
 
@@ -20,24 +20,33 @@ export const Form: React.FC = () => {
                 <div className='inputs' >
                     <input
                         placeholder='First name'
-                        {...register("firstName", { required: 'Error' })} />
+                        {...register("firstName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
+                    {errors.firstName && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
                     <input
                         placeholder='Last name'
-                        {...register("lastName")} />
+                        {...register("lastName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
+                    {errors.lastName && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
+                    <input
+                        type='tel'
+                        placeholder='Phone'
+                        {...register("phone", { required: true, pattern: /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/ })} />
+                    {errors.email && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
                     <input
                         placeholder='Email'
-                        type="email" {...register("email")} />
+                        {...register("email", { required: true, pattern: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/ })} />
+                    {errors.email && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
                     <input
-                        placeholder='Phone number'
-                        {...register("phone")} />
-                    <input
+                        type='password'
                         placeholder='Password'
-                        type='password'{...register("password")} />
+                        {...register("password", { required: true })} />
+                    {errors.password && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
                     <input
+                        type='password'
                         placeholder='Confirm password'
-                        type="password" {...register("confirmPassword")} />
-                    <input className='checkbox' type="checkbox" />
-                    <Button onClick={() => onSubmit} label='Registration' />
+                        {...register("confirmPassword", { required: true })} />
+                    {errors.confirmPassword && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
+                    <input type="checkbox" style={{ marginLeft: '-95%' }} />
+                    <input type='submit' />
                 </div>
             </form>
         </div>
--- src/interface/interfaces.ts
+++ src/interface/interfaces.ts
@@ -1,15 +1,3 @@
-export interface IInputs {
-    type: string,
-    value?: string,
-    onInput?: () => void,
-    className?: string
-}
-
-export interface IButton {
-    label: string,
-    onClick: () => void
-}
-
 export type IFormValue = {
     firstName: string,
     lastName: string,
