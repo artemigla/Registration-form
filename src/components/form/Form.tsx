af8514a5b9093fa43ad4ec40c8fc4471a7d82b13
artemigla <igla.artem@gmail.com>
2024-01-26 18:15:12 +0200

    Added icons to the input field.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { IFormValue } from '../../interface/interfaces';
 import './style.css';
@@ -6,10 +6,18 @@ import './style.css';
 export const Form: React.FC = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm<IFormValue>({
-        mode: 'onChange'
+        mode: 'onSubmit'
     });
 
     const onSubmit: SubmitHandler<IFormValue> = (data) => console.log(data);
+
+    const [isPassword, setIsPassword] = useState(false)
+    const [isConfirmPassword, setIsConfirmPassword] = useState(false);
+    const [isCheckbox, setIsCheckbox] = useState(false);
+
+    const handlePassword = () => setIsPassword(!isPassword);
+    const handleConfirmPassword = () => setIsConfirmPassword(!isConfirmPassword);
+    const handleCheckbox = () => setIsCheckbox(!isCheckbox);
 
     return (
         <div className='container'>
@@ -18,35 +26,68 @@ export const Form: React.FC = () => {
             </div>
             <form className='wrapper' onSubmit={handleSubmit(onSubmit)}>
                 <div className='inputs' >
-                    <input
-                        placeholder='First name'
-                        {...register("firstName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
-                    {errors.firstName && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input
-                        placeholder='Last name'
-                        {...register("lastName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
-                    {errors.lastName && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input
-                        type='tel'
-                        placeholder='Phone'
-                        {...register("phone", { required: true, pattern: /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/ })} />
-                    {errors.email && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input
-                        placeholder='Email'
-                        {...register("email", { required: true, pattern: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/ })} />
-                    {errors.email && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input
-                        type='password'
-                        placeholder='Password'
-                        {...register("password", { required: true })} />
-                    {errors.password && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input
-                        type='password'
-                        placeholder='Confirm password'
-                        {...register("confirmPassword", { required: true })} />
-                    {errors.confirmPassword && <p style={{ color: 'red', marginLeft: '-65%' }}>Field is required</p>}
-                    <input type="checkbox" style={{ marginLeft: '-95%' }} />
-                    <input type='submit' />
+                    <div className='inputcontent'>
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                        <input
+                            placeholder='First name'
+                            {...register("firstName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
+                        {errors.firstName && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='inputcontent'>
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                        <input
+                            placeholder='Last name'
+                            {...register("lastName", { required: true, pattern: /^([A-Z][a-z\-']{2,17})|([А-ЯЁIЇҐЄ][а-яёіїґє\-']{2,17})$/ })} />
+                        {errors.lastName && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='inputcontent'>
+                        <i className="fa fa-phone-square" style={{ fontSize: '24px' }}></i>
+                        <input
+                            type='tel'
+                            placeholder='Phone'
+                            {...register("phone", { required: true, pattern: /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/ })} />
+                        {errors.email && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='inputcontent'>
+                        <i className="fa fa-envelope" style={{ fontSize: '24px' }}></i>
+                        <input
+                            placeholder='Email'
+                            {...register("email", { required: true, pattern: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/ })} />
+                        {errors.email && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='inputcontent'>
+                        <i className="fa fa-key" style={{ fontSize: '24px' }}></i>
+                        <input
+                            type={isPassword === false ? 'password' : 'text'}
+                            placeholder='Password'
+                            {...register("password", { required: true })} />
+                        {isPassword === false ?
+                            <i className="fa fa-eye-slash" onClick={handlePassword} style={{ fontSize: '24px' }}></i>
+                            :
+                            <i className="fa fa-eye" onClick={handlePassword} style={{ fontSize: '24px' }}></i>}
+                        {errors.password && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='inputcontent'>
+                        <i className="fa fa-key" style={{ fontSize: '24px' }}></i>
+                        <input
+                            type={isConfirmPassword === false ? 'password' : 'text'}
+                            placeholder='Confirm password'
+                            {...register("confirmPassword", { required: true })} />
+                        {isConfirmPassword === false ?
+                            <i className="fa fa-eye-slash" onClick={handleConfirmPassword} style={{ fontSize: '24px' }}></i>
+                            :
+                            <i className="fa fa-eye" onClick={handleConfirmPassword} style={{ fontSize: '24px' }}></i>}
+                        {errors.confirmPassword && <p style={{ color: 'red' }}>Field is required</p>}
+                    </div>
+                    <div className='checkbox'>
+                        {isCheckbox ?
+                            <i onClick={handleCheckbox} className="fa fa-check-square-o" style={{ fontSize: '32px' }}></i>
+                            :
+                            <i onClick={handleCheckbox} className="fa fa-square-o" style={{ fontSize: '32px' }}></i>}
+                    </div>
+                    <div className='btn'>
+                        <button>Send</button>
+                    </div>
                 </div>
             </form>
         </div>
